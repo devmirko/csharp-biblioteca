dd9828dc131af7d39820067e618438f7793ae870
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-11-04 16:00:50 +0100

    struttura classe Documenti

--- Program.cs
+++ Program.cs
@@ -26,3 +26,60 @@ Console.WriteLine("Hello, World!");
 
 //L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
 //Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+
+Console.WriteLine("benvenuti nella biblioteca");
+Console.WriteLine("cerca il tuo libro");
+
+public class Documenti
+{
+    public string Titolo { get; set; }
+    public int Anno { get; set; }
+    public string Settore { get; set; }
+    public bool Stato { get; set; }
+    public string Scaffale { get; set; }
+    public string Autore { get; set; }
+
+    //costruttore
+    public Documenti(string titolo, int anno, string settore, bool stato, string scaffale, string autore)
+    {
+        Titolo = titolo;
+        Anno = anno;
+        Settore = settore;
+        Stato = stato;
+        Scaffale = scaffale;
+        Autore = autore;
+
+
+
+
+
+
+    }
+}
+
+
+public class Libri : Documenti
+{
+    public int NumeroPagine { get; set; }
+
+    public Libri(string titolo, int anno, string settore, bool stato, string scaffale, string autore, int numeroPagine) : base(titolo, anno, settore, stato, scaffale, autore)
+    {
+        NumeroPagine = numeroPagine;
+
+    }
+
+}
+
+
+public class Dvd : Documenti
+{
+    public int Durata { get; set; }
+
+    public Dvd(string titolo, int anno, string settore, bool stato, string scaffale, string autore, int durata) : base(titolo, anno, settore, stato, scaffale, autore)
+    {
+        Durata = durata;
+
+
+    }
+
+}
